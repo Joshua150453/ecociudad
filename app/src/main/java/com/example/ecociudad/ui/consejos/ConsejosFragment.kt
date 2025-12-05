fcfe96bab1ff352ed5c4d76ac8f07ff64a70bb10
Joshua150453 <jortizr@ulasalle.edu.pe>
2025-12-04 20:09:24 -0500

    Actualizando fragment de consejos

--- app/src/main/java/com/example/ecociudad/ui/consejos/ConsejosFragment.kt
+++ app/src/main/java/com/example/ecociudad/ui/consejos/ConsejosFragment.kt
@@ -1,19 +1,35 @@
 package com.example.ecociudad.ui.consejos
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.widget.Toast
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ecociudad.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ecociudad.data.ConsejosData
+import com.example.ecociudad.databinding.FragmentConsejosBinding
 
 class ConsejosFragment : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private var _binding: FragmentConsejosBinding? = null
+    private val binding get() = _binding!!
 
-        Toast.makeText(requireContext(), "Mostrando consejos ecológicos...", Toast.LENGTH_SHORT).show()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentConsejosBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.recyclerConsejos.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerConsejos.adapter = ConsejoAdapter(ConsejosData.listaConsejos)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
